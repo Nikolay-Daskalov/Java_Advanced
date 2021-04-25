21162d138ab5b85af082a76262b28e0992c50773
Kmetamazter <daskalovnikolaj@gmail.com>
2021-04-25 06:15:45 +0300

    Update main

--- Stacks and Queues/p02_DecimalToBinary/src/Main.java
+++ Stacks and Queues/p02_DecimalToBinary/src/Main.java
@@ -1,5 +1,28 @@
+import java.util.ArrayDeque;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        
+        Scanner scanner = new Scanner(System.in);
+
+        int number = Integer.parseInt(scanner.nextLine());
+
+        if (number == 0) {
+            System.out.println(0);
+            return;
+        }
+
+        ArrayDeque<Integer> binary = new ArrayDeque<>();
+        while (number != 0) {
+            int zeroOrOne = number % 2;
+            binary.push(zeroOrOne);
+            number /= 2;
+        }
+
+        while (!binary.isEmpty()) {
+            System.out.print(binary.pop());
+        }
+
+        System.out.println();
     }
 }
