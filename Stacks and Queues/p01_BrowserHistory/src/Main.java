cf79c30d7dfdbe8e971aa979b7dd82c8cc610d52
Kmetamazter <daskalovnikolaj@gmail.com>
2021-04-25 06:00:30 +0300

    Update Main.java

--- Stacks and Queues/p01_BrowserHistory/src/Main.java
+++ Stacks and Queues/p01_BrowserHistory/src/Main.java
@@ -1,5 +1,29 @@
+import java.util.ArrayDeque;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        
+        Scanner scanner = new Scanner(System.in);
+
+        ArrayDeque<String> test = new ArrayDeque<>();
+
+        String input = scanner.nextLine();
+
+        while (!input.equals("Home")) {
+
+            if (input.equals("back")) {
+                if (test.size() <= 1) {
+                    System.out.println("no previous URLs");
+                } else {
+                    test.pop();
+                    System.out.println(test.peek());
+                }
+            } else {
+                System.out.println(input);
+                test.push(input);
+            }
+
+            input = scanner.nextLine();
+        }
     }
 }
