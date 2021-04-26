552790c8aa717ba7a7b9b0de288d1e7e2aefb0dd
Kmetamazter <daskalovnikolaj@gmail.com>
2021-04-26 08:14:51 +0300

    Update Main.java

--- Stacks and Queues/p04_PrinterQueue/src/Main.java
+++ Stacks and Queues/p04_PrinterQueue/src/Main.java
@@ -1,5 +1,31 @@
+import java.util.ArrayDeque;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
+        ArrayDeque<String> printer = new ArrayDeque<>();
+
+        String input = scanner.nextLine();
+
+        while (!input.equals("print")) {
+
+            if (input.equals("cancel")) {
+                if (printer.isEmpty()) {
+                    System.out.println("Printer is on standby");
+                } else {
+                    System.out.println("Canceled " + printer.poll());
+                }
+            } else {
+                printer.offer(input);
+            }
+
+            input = scanner.nextLine();
+        }
+
+        for (String string : printer) {
+            System.out.println(string);
+        }
     }
 }
