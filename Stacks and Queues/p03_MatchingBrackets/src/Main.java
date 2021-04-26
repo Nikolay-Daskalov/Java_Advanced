374ce7de2d1ffb8b521c9593b7e65939165ddce1
Kmetamazter <daskalovnikolaj@gmail.com>
2021-04-26 08:10:56 +0300

    Update Main.java

--- Stacks and Queues/p03_MatchingBrackets/src/Main.java
+++ Stacks and Queues/p03_MatchingBrackets/src/Main.java
@@ -1,5 +1,20 @@
+import java.util.ArrayDeque;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        
+        Scanner scanner = new Scanner(System.in);
+
+        ArrayDeque<Integer> arrayDeque = new ArrayDeque<>();
+
+        String input = scanner.nextLine();
+
+        for (int i = 0; i < input.length(); i++) {
+            if (input.charAt(i) == '(') {
+                arrayDeque.push(i);
+            } else if (input.charAt(i) == ')') {
+                System.out.println(input.substring(arrayDeque.pop(), i + 1));
+            }
+        }
     }
 }
