fb80ecbc338510a835ba97cc91805eb00c599594
Kmetamazter <daskalovnikolaj@gmail.com>
2021-04-28 08:10:50 +0300

    Update Main.java

--- Multidimensional Arrays/p02_PositionsOf/src/Main.java
+++ Multidimensional Arrays/p02_PositionsOf/src/Main.java
@@ -1,5 +1,39 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        
+        Scanner scanner = new Scanner(System.in);
+
+        int[] rolsAndCols = ReadArray(scanner, "\\s+");
+
+        int rows = rolsAndCols[0];
+        int cols = rolsAndCols[1];
+
+        int[][] matrix = new int[rows][cols];
+
+        for (int row = 0; row < matrix.length; row++) {
+            matrix[row] = ReadArray(scanner, "\\s+");
+        }
+
+        int n = Integer.parseInt(scanner.nextLine());
+
+        for (int row = 0; row < matrix.length; row++) {
+            int[] currentArray = matrix[row];
+            for (int col = 0; col < currentArray.length; col++) {
+                if (currentArray[col] == n) {
+                    System.out.printf("The position is: %d %d%.n", row, col);
+                    return;
+                }
+            }
+        }
+
+        System.out.println("not found");
+    }
+
+    private static int[] ReadArray(Scanner scanner, String s) {
+        return Arrays.stream(scanner.nextLine()
+                .split(s))
+                .mapToInt(Integer::parseInt).toArray();
     }
 }
