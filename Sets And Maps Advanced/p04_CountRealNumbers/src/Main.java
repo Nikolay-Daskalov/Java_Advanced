a0909fca0f1c415c79bdb058017f2488333934f6
Kmetamazter <daskalovnikolaj@gmail.com>
2021-04-30 00:59:54 +0300

    commit

--- Sets And Maps Advanced/p04_CountRealNumbers/src/Main.java
+++ Sets And Maps Advanced/p04_CountRealNumbers/src/Main.java
@@ -1,5 +1,19 @@
+import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
+        Map<Double, Integer> realNumbersCount = new LinkedHashMap<>();
+        Arrays.stream(scanner.nextLine().split("\\s+"))
+                .mapToDouble(Double::parseDouble)
+                .forEach(number -> {
+                    realNumbersCount.putIfAbsent(number, 0);
+                    realNumbersCount.put(number, realNumbersCount.get(number) + 1);
+                });
+
+        for (Map.Entry<Double, Integer> entry : realNumbersCount.entrySet()) {
+            System.out.printf("%.1f -> %d%n", entry.getKey(), entry.getValue());
+        }
     }
 }
