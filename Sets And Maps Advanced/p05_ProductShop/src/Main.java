120bea95899df36f4dde62278732e15f6b9c7f0d
Kmetamazter <daskalovnikolaj@gmail.com>
2021-05-01 16:53:13 +0300

    Update Main.java

--- Sets And Maps Advanced/p05_ProductShop/src/Main.java
+++ Sets And Maps Advanced/p05_ProductShop/src/Main.java
@@ -1,5 +1,25 @@
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.TreeMap;
+
 public class Main {
     public static void main(String[] args) {
-        
+        Scanner scanner = new Scanner(System.in);
+
+        String[] tokens = scanner.nextLine().split(",\\s+");
+        Map<String, Map<String, Double>> shops = new TreeMap<>();
+
+        while (!tokens[0].equals("Revision")) {
+
+            String shop = tokens[0];
+            String product = tokens[1];
+            double price = Double.parseDouble(tokens[2]);
+
+            shops.putIfAbsent(shop, new LinkedHashMap<>());
+            shops.get(shop).put(product, price);
+
+            tokens = scanner.nextLine().split(",\\s+");
+        }
     }
 }
