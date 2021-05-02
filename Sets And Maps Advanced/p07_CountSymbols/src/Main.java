a15b19610853302d4d4bb5efe72c939cf9667512
Kmetamazter <daskalovnikolaj@gmail.com>
2021-05-02 08:08:13 +0300

    commit

--- Sets And Maps Advanced/p07_CountSymbols/src/Main.java
+++ Sets And Maps Advanced/p07_CountSymbols/src/Main.java
@@ -1,5 +1,22 @@
+import java.util.Map;
+import java.util.Scanner;
+import java.util.TreeMap;
+
 public class Main {
     public static void main(String[] args) {
-        
+        Scanner scanner = new Scanner(System.in);
+
+        String input = scanner.nextLine();
+        Map<Character, Integer> map = new TreeMap<>();
+
+        for (int i = 0; i < input.length(); i++) {
+            char current = input.charAt(i);
+            map.putIfAbsent(current, 0);
+            map.put(current, map.get(current) + 1);
+        }
+
+        map.forEach((key, value) -> {
+            System.out.printf("%c: %d time/s%n", key, value);
+        });
     }
 }
