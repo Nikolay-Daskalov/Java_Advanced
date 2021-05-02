f7f271598c0f7088ea52a03e9e4fc56f01a3629b
Kmetamazter <daskalovnikolaj@gmail.com>
2021-05-02 08:04:11 +0300

    commit

--- Sets And Maps Advanced/p06_UniqueUsernames/src/Main.java
+++ Sets And Maps Advanced/p06_UniqueUsernames/src/Main.java
@@ -1,5 +1,19 @@
+import java.util.LinkedHashSet;
+import java.util.Scanner;
+import java.util.Set;
+
 public class Main {
     public static void main(String[] args) {
-        
+        Scanner scanner = new Scanner(System.in);
+
+        int n = Integer.parseInt(scanner.nextLine());
+        Set<String> uniqueUsernames = new LinkedHashSet<>();
+
+        while (n-- > 0) {
+            String input = scanner.nextLine();
+            uniqueUsernames.add(input);
+        }
+
+        uniqueUsernames.forEach(System.out::println);
     }
 }
