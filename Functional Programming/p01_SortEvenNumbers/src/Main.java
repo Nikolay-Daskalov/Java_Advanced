8fb0dc95cf497a944a16468bbbc8cc69529275e2
Kmetamazter <daskalovnikolaj@gmail.com>
2021-05-03 10:35:00 +0300

    commit

--- Functional Programming/p01_SortEvenNumbers/src/Main.java
+++ Functional Programming/p01_SortEvenNumbers/src/Main.java
@@ -1,5 +1,24 @@
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
 public class Main {
     public static void main(String[] args) {
-        
+        Scanner scanner = new Scanner(System.in);
+
+        String collect = null;
+        try {
+            collect = Arrays.stream(scanner.nextLine().split("\\s+"))
+                    .mapToDouble(Double::parseDouble)
+                    .filter(number -> number % 2 == 0)
+                    .sorted()
+                    .mapToObj(String::valueOf)
+                    .collect(Collectors.joining(", "));
+        } catch (Exception e) {
+            System.out.println("Enter valid input next time!");
+            return;
+        }
+
+        System.out.println("The even numbers are: " + (collect.isEmpty() ? "none" : collect));
     }
 }
