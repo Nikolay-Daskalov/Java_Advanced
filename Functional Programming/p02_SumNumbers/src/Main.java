48df2d240f1b41f6a2d9a2b7a9ca91fd035d29b4
Kmetamazter <daskalovnikolaj@gmail.com>
2021-05-03 10:40:03 +0300

    Update Main.java

--- Functional Programming/p02_SumNumbers/src/Main.java
+++ Functional Programming/p02_SumNumbers/src/Main.java
@@ -1,5 +1,17 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        
+        Scanner scanner = new Scanner(System.in);
+
+        int[] numbers = Arrays.stream(scanner.nextLine().split(",\\s+"))
+                .mapToInt(Integer::parseInt)
+                .toArray();
+
+        System.out.println("Count = " + Arrays.stream(numbers).count());
+        System.out.println("Sum = " + Arrays.stream(numbers).sum());
+
+        scanner.close();
     }
 }
