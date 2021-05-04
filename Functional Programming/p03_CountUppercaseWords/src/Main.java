21c2dc9c8413fd3b1cf5d5b464aeac811e11b7dc
Kmetamazter <daskalovnikolaj@gmail.com>
2021-05-04 09:29:19 +0300

    Update Main.java

--- Functional Programming/p03_CountUppercaseWords/src/Main.java
+++ Functional Programming/p03_CountUppercaseWords/src/Main.java
@@ -1,5 +1,21 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
 public class Main {
     public static void main(String[] args) {
-        
+        Scanner scanner = new Scanner(System.in);
+
+        List<String> collect = Arrays.stream(scanner.nextLine().split("\\s+"))
+                .filter(s -> Character.isUpperCase(s.charAt(0)))
+                .collect(Collectors.toList());
+
+        System.out.println(collect.size());
+        for (String string : collect) {
+            System.out.println(string);
+        }
+
+        scanner.close();
     }
 }
