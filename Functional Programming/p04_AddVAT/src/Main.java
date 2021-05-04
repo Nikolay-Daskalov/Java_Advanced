5a3028438f31513e609e7a4e66c7bca0d3957dfd
Kmetamazter <daskalovnikolaj@gmail.com>
2021-05-04 10:03:46 +0300

    commit

--- Functional Programming/p04_AddVAT/src/Main.java
+++ Functional Programming/p04_AddVAT/src/Main.java
@@ -1,5 +1,20 @@
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.function.UnaryOperator;
+
 public class Main {
     public static void main(String[] args) {
-        
+        Scanner scanner = new Scanner(System.in);
+
+        UnaryOperator<Double> unaryOperator = vat -> vat * 1.20;
+        System.out.println("Prices with VAT:");
+        Arrays.stream(scanner.nextLine().split(",\\s+"))
+                .map(Double::parseDouble)
+                .map(unaryOperator)
+                .forEach(d -> {
+                    System.out.printf("%.2f%n", d);
+                });
+
+        scanner.close();
     }
 }
