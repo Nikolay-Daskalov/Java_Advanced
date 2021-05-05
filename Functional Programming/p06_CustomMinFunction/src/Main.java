5c0661b314af582dc8b9868e01bd5aca02c936d1
Kmetamazter <daskalovnikolaj@gmail.com>
2021-05-05 10:51:31 +0300

    Update Main.java

--- Functional Programming/p06_CustomMinFunction/src/Main.java
+++ Functional Programming/p06_CustomMinFunction/src/Main.java
@@ -1,5 +1,15 @@
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.function.Function;
+
 public class Main {
     public static void main(String[] args) {
-        
+        Scanner scanner = new Scanner(System.in);
+
+        Function<int[], Integer> function = numbers -> Arrays.stream(numbers).min().getAsInt();
+
+        System.out.println(function.apply(Arrays.stream(scanner.nextLine().split("\\s+")).mapToInt(Integer::parseInt).toArray()));
+
+        scanner.close();
     }
 }
