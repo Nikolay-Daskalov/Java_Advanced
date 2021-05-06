dcda9416467bc2674c7f7a38e53185f69167929f
Kmetamazter <daskalovnikolaj@gmail.com>
2021-05-06 11:19:21 +0300

    Update Main.java

--- Functional Programming/p08_PredicateForNames/src/Main.java
+++ Functional Programming/p08_PredicateForNames/src/Main.java
@@ -1,5 +1,19 @@
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.function.Predicate;
+
 public class Main {
     public static void main(String[] args) {
-        
+        Scanner scanner = new Scanner(System.in);
+
+        int n = Integer.parseInt(scanner.nextLine());
+
+        Predicate<String> predicate = s -> s.length() <= n;
+
+        Arrays.stream(scanner.nextLine().split("\\s+"))
+                .filter(predicate)
+                .forEach(System.out::println);
+
+        scanner.close();
     }
 }
