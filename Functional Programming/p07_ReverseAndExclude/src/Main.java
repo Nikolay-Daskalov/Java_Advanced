028d3722aa6cfa812fbc3a2550969f84fee0f92f
Kmetamazter <daskalovnikolaj@gmail.com>
2021-05-06 11:16:16 +0300

    Update Main.java

--- Functional Programming/p07_ReverseAndExclude/src/Main.java
+++ Functional Programming/p07_ReverseAndExclude/src/Main.java
@@ -1,5 +1,25 @@
+import java.util.*;
+import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+
 public class Main {
     public static void main(String[] args) {
-        
+        Scanner scanner = new Scanner(System.in);
+
+        List<Integer> list = Arrays.stream(scanner.nextLine().split("\\s+"))
+                .map(Integer::parseInt).collect(Collectors.toList());
+
+        int n = Integer.parseInt(scanner.nextLine());
+
+        Collections.reverse(list);
+
+        Predicate<Integer> predicate = integer -> integer % n != 0;
+
+        list.stream()
+                .filter(predicate)
+                .forEach(integer -> System.out.print(integer + " "));
+
+        scanner.close();
     }
 }
