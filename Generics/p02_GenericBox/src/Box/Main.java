6d28eb9f76fdf9dfacc441bc4fc4cc155f8b5ba7
Kmetamazter <daskalovnikolaj@gmail.com>
2021-05-07 11:38:44 +0300

    commit

--- Generics/p02_GenericBox/src/Box/Main.java
+++ Generics/p02_GenericBox/src/Box/Main.java
@@ -6,7 +6,7 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) throws IOException {// Main
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-
+        
         int n = Integer.parseInt(reader.readLine());
 
         for (int i = 0; i < n; i++) {
@@ -14,7 +14,6 @@ public class Main {
             Box<String> box = new Box<>(input);
             System.out.println(box.toString());
         }
-
         reader.close();
     }
 }
