82696d2f6a4ba29e52db1cd0ca8e20a1e1c7297b
Kmetamazter <daskalovnikolaj@gmail.com>
2021-05-09 15:02:13 +0300

    commit

--- Iterators and Comparators/p01_Library/src/Book.java
+++ Iterators and Comparators/p01_Library/src/Book.java
@@ -2,7 +2,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-public class Book {
+public class Book implements Comparable<Book> {
     private String title;
     private int year;
     private List<String> authors;
@@ -36,5 +36,23 @@ public class Book {
     private void setAuthors(String[] authors) {
         this.authors = new ArrayList<>();
         this.authors.addAll(Arrays.asList(authors));
+    }
+
+    @Override
+    public int compareTo(Book other) {
+        int c = this.getTitle().compareTo(other.getTitle());
+        if (c == 0) {
+            c = Integer.compare(this.getYear(), other.getYear());
+        }
+        return c;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "title='" + title + '\'' +
+                ", year=" + year +
+                ", authors=" + authors +
+                '}';
     }
 }
