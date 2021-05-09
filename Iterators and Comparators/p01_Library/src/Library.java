bf5b5bb2f2be2c58a20c23370e4fe039aa510184
Kmetamazter <daskalovnikolaj@gmail.com>
2021-05-09 14:48:58 +0300

    commit

--- Iterators and Comparators/p01_Library/src/Library.java
+++ Iterators and Comparators/p01_Library/src/Library.java
@@ -1,3 +1,34 @@
-public class Library {
-    
+import java.util.Iterator;
+
+public class Library implements Iterable<Book> {
+    private Book[] books;
+
+
+    public Library(Book... books) {
+        this.setBooks(books);
+    }
+
+    private void setBooks(Book[] books) {
+        this.books = books;
+    }
+
+    @Override
+    public Iterator<Book> iterator() {
+        return new LibraryIterator();
+    }
+
+    private class LibraryIterator implements Iterator<Book> {
+
+        private int index = 0;
+
+        @Override
+        public boolean hasNext() {
+            return index < books.length;
+        }
+
+        @Override
+        public Book next() {
+            return books[index++];
+        }
+    }
 }
--- Iterators and Comparators/p01_Library/src/Main.java
+++ Iterators and Comparators/p01_Library/src/Main.java
@@ -1,3 +1,4 @@
+import java.util.Iterator;
 import java.util.List;
 
 public class Main {
@@ -5,6 +6,10 @@ public class Main {
         Book bookOne = new Book("Animal Farm", 2003, "George Orwell");
         Book bookThree = new Book("The Documents in the Case", 2002);
         Book bookTwo = new Book("The Documents in the Case", 1930, "Dorothy Sayers", "Robert Eustace");
-        System.out.println();
+        Library library = new Library(bookOne, bookTwo, bookThree);
+
+        for (Book book : library) {
+            System.out.println(book.getTitle());
+        }
     }
 }
